4aefaa68637cde064aa24620575b0e91e9a703ba
Elias Ortega <eliasortega2011@gmail.com>
2023-04-27 22:02:39 -0400

    for, for of, forEach, intro filter

--- assets/js/scriptDos.js
+++ assets/js/scriptDos.js
@@ -5,7 +5,35 @@ function consumoDeAPI() {
         url: "https://pokeapi.co/api/v2/pokemon",
         success: function (response) {
            console.log(response);
+           /* for(var i = 0; i < response.results.length; i++){
+                console.log(response.results[i]);
+           } */
 
+           /* For of recorre arreglos. se declara una variable temporal */
+           for(var pokemon of response.results){
+                console.log(pokemon.url);
+           }
+
+           /* forEach, map, filter */
+           
+           /* Con definicion de funcion anterior */
+           function callback(pokemon, index){
+                console.log(pokemon +  " " + index);
+           }  
+
+           response.results.forEach(callback);  
+
+           /* Con funcion anonima */
+           response.results.forEach(function(pokemon, index){
+                console.log(pokemon +  " " + index);
+           });   
+          
+           /* Filter */
+            var filtrados =response.results.filter(function(pokemon , index){
+                return pokemon.name =='metapod';
+                
+           });
+           console.log(filtrados);
         }
     });
 
