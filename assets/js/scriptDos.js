e1ef91f9ccecefda8595b8a0ca0c017074d6379d
Elias Ortega <eliasortega2011@gmail.com>
2023-04-27 20:22:52 -0400

    iniciando metodos avanzados de arreglos

---
+++ assets/js/scriptDos.js
@@ -0,0 +1,15 @@
+function consumoDeAPI() {
+
+    $.ajax({
+        type: "get",
+        url: "https://pokeapi.co/api/v2/pokemon",
+        success: function (response) {
+           console.log(response);
+
+        }
+    });
+
+}
+
+consumoDeAPI();
+
