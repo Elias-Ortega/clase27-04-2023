b5fce7df96045d121ebebf7b67b471eb91860b51
Elias Ortega <eliasortega2011@gmail.com>
2023-04-28 20:47:00 -0400

    scriptTres.js Map

--- assets/js/scriptTres.js
+++ assets/js/scriptTres.js
@@ -6,9 +6,9 @@ function consumoDeAPI() {
         type: "get",
         url: "https://pokeapi.co/api/v2/pokemon",
         dataType: "json",
-        async:false,
+        async: false,
         success: function (response) {
-             arregloPokemones =  consumoDeAPIPorPokemon(response.results);
+            arregloPokemones = consumoDeAPIPorPokemon(response.results);
         }
     });
     return arregloPokemones
@@ -21,7 +21,7 @@ function consumoDeAPIPorPokemon(arregloPokemonURL) {
             type: "get",
             url: pokemon.url,
             dataType: "json",
-            async:false,
+            async: false,
             success: function (response) {
                 arregloPokemonesInfoCompleta.push(response);
             }
@@ -34,9 +34,9 @@ console.log(consumoDeAPI());
 
 
 /* Filter */
-function filtrarPorBaseExperiencia(arregloPokemones){
-     var arregloFiltrado = arregloPokemones.filter(function(pokemon){
-            return pokemon.base_experience > 100;
+function filtrarPorBaseExperiencia(arregloPokemones) {
+    var arregloFiltrado = arregloPokemones.filter(function (pokemon) {
+        return pokemon.base_experience > 100;
 
     });
     console.log(arregloFiltrado);
@@ -44,3 +44,24 @@ function filtrarPorBaseExperiencia(arregloPokemones){
 
 var arregloPokemones = consumoDeAPI();
 filtrarPorBaseExperiencia(arregloPokemones);
+
+/* Map */
+function aumentarBaseExperiencia(arregloPokemones) {
+    var arregloNuevo = arregloPokemones.map(function (pokemon) {
+        return pokemon.base_experience * 1.2;
+    });
+    console.log(arregloNuevo);
+}
+
+function aumentarBaseExperienciaNuevoAtributo(arregloPokemones) {
+    arregloPokemones.map(function(pokemon) {
+        return pokemon.base_experience_aumentado = pokemon.base_experience * 1.2;
+    });
+    return arregloPokemones;
+}
+
+
+aumentarBaseExperiencia(arregloPokemones);
+var arregloConNuevoAtributo = aumentarBaseExperienciaNuevoAtributo(arregloPokemones);
+console.log(arregloConNuevoAtributo);
+
